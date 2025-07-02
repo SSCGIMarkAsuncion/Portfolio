08ffbf9560946d4f4ec50ec8e70675ef865ca25a
Mark Anthony Asuncion <maasuncion@sscgi.com>
2025-07-02 14:59:31 +0800

    Nav responsive

--- src/index.js
+++ src/index.js
@@ -1,7 +1,3 @@
-function clamp(v, min, max) {
-    return Math.min(max, Math.max(v, min));
-}
-
 function disableImageDrag() {
     const imgs = document.querySelectorAll("img[draggable=\"false\"");
     imgs.forEach((v) => v.ondragstart = (e) => {
@@ -10,64 +6,34 @@ function disableImageDrag() {
     });
 }
 
-function initDraggableGallery() {
-    /** @type {HTMLDivElement | null} el */
-    const el = document.querySelector(".draggable-gallery");
-
-    /** @type {HTMLDivElement | null} scrollable */
-    const scrollable = document.querySelector(".draggable-gallery:first-child");
-    if (!el || !scrollable)
+function initNavlinks() {
+    /** @type {HTMLDivElement | null} el  */
+    const el = document.querySelector(".open-nav-links");
+    if (!el)
         return;
 
-    let isDragging = false;
-    const scrollX = 0;
-    let mouseXStart = 0;
-    let scrollLeftStart = 0;
-    let currentScrollLeft = 0;
+    /** @type {HTMLDivElement | null} el  */
+    const target = document.querySelector(`#${el.dataset.openTarget}`);
 
-    scrollable.style.transform = `translateX(${currentScrollLeft}%)`;
-
-    /** @param {MouseEvent} e */
-    el.onmousedown = (e) => {
-        e.preventDefault();
+    el.onclick = (e) => {
         e.stopPropagation();
 
-        mouseXStart = e.clientX;
-        // scrollLeftStart = el.scrollLeft;
-        scrollLeftStart = currentScrollLeft;
-        isDragging = true;
-        console.log("start", mouseXStart, scrollLeftStart);
-    };
+        target.classList.toggle("modal");
+        target.classList.toggle("open");
+    }
 
-    /** @param {MouseEvent} e */
-    el.onmousemove = (e) => {
-        if (!isDragging)
-            return;
-        e.preventDefault();
+    const btnClose = target.querySelector("button");
+    const closeFn = (e) => {
         e.stopPropagation();
 
-        const maxScrollLeft = el.scrollWidth - el.clientWidth;
-        let scroll = (mouseXStart - e.clientX) * 1;
-        scroll = scrollLeftStart + scroll;
-        // scroll = scrollLeftStart - scroll;
-        
-        console.log("scrollV", scrollLeftStart + scroll, maxScrollLeft);
-        currentScrollLeft = scroll;
-        scrollable.style.transform = `translateX(${-scroll}px)`;
-        // el.scrollLeft = scroll;
+        target.classList.remove("modal");
+        target.classList.remove("open");
     };
-
-    /** @param {MouseEvent} e */
-    el.onmouseup = (e) => {
-        e.preventDefault();
-        e.stopPropagation();
-
-        isDragging = false;
-        console.log(el.scrollLeft);
-    };
+    target.onclick = closeFn;
+    btnClose.onclick = closeFn;
 }
 
 window.onload = () => {
     disableImageDrag();
-    // initDraggableGallery();
+    initNavlinks();
 }
