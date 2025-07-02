7b6a384ca6a23f54a8b22880cb485533915f8200
Mark Anthony Asuncion <maasuncion@sscgi.com>
2025-07-02 16:36:01 +0800

    Add back-to-top | Minor css fixes

--- src/index.js
+++ src/index.js
@@ -33,7 +33,26 @@ function initNavlinks() {
     btnClose.onclick = closeFn;
 }
 
+function initBackToTopBtn() {
+    const el = document.querySelector(".back-to-top");
+    if (!el)
+        return;
+
+    window.onscroll = (e) => {
+        const docHeight = document.documentElement.scrollHeight - window.innerHeight;
+        const scrollPercent = (window.scrollY/docHeight) * 100;
+        if (scrollPercent < 15) {
+            el.classList.replace("show", "hide");
+            return;
+        }
+
+        el.classList.replace("hide", "show");
+        console.log(docHeight, scrollPercent);
+    };
+}
+
 window.onload = () => {
     disableImageDrag();
     initNavlinks();
+    initBackToTopBtn();
 }
